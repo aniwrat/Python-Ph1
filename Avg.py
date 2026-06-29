67210013cbe2241433a466fdd58cf85f45478aaf
Aniwrat Srivastava <kidsfun@KIDSs-MacBook-Pro.local>
2026-06-29 12:59:14 +0530

    My first code

---
+++ Avg.py
@@ -0,0 +1,5 @@
+a=10
+b=20
+c=30
+avg=(a+b+c)/3
+print("The average of a, b, and c is:", avg)
---
+++ check_python.py
@@ -0,0 +1,4 @@
+#check whether python is present in I love Python programming or not
+s="I love Python programming"
+check="Python"
+print(check in s)
